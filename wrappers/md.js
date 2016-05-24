0ee2091aa2ed30d5c8c27316840e4852e6961b98
Jon Gold <hello@designedbygold.com>
2016-05-24 19:41:25 +0100

    add plzdontshare section

--- wrappers/md.js
+++ wrappers/md.js
@@ -5,6 +5,15 @@ import ReadNext from '../components/ReadNext'
 import { config } from 'config'
 import Bio from 'components/Bio'
 
+function PlzDontShare () {
+  return (
+    <div className="pa3 bg-light-gray measure bg-yellow lh-copy mono b">
+      this post hasn’t been published yet - if you’ve stumbled upon it I’d love
+      your feedback but please don’t share it just yet!
+    </div>
+  )
+}
+
 class MarkdownWrapper extends React.Component {
   render () {
     const { route } = this.props
@@ -19,10 +28,11 @@ class MarkdownWrapper extends React.Component {
                {moment(post.date).format('MMMM D, YYYY')}
               </time>
               <span className="f6 ttu tracked gray">
-                { post.wordcount && `${Math.ceil(post.wordcount / 200)} min read` }
+                { post.wordCount && `${Math.ceil(post.wordCount / 200)} min read` }
               </span>
               <h1 className="lh-title fw8 f3 f2-ns measure">{post.title}</h1>
             </header>
+            { (post.published === false) && <PlzDontShare /> }
             <div
               className="mt4 mt0-ns measure lh-copy f6 f5-ns "
               dangerouslySetInnerHTML={{ __html: post.body }}
